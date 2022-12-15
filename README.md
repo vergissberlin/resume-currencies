e330493f6603fbfbdfb8e0de4f0687a540590024
André Lademann <andre.lademann@netresearch.de>
2022-12-15 02:37:58 +0100

    Add base currency selector

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-https://www.solarsystemscope.com/textures/
+## Credits
+
+- Textures https://www.solarsystemscope.com/textures/
+- API https://app.freecurrencyapi.com/dashboard
+
+
+```js
+// Place the currency values on the earth, hide labels behind the earth
+const placeCurrency = async () => {
+    // Get the currency data
+    const currency = await getCurrency()
+
+    // Loop through the currency data
+    for (const [key, value] of Object.entries(currency)) {
+
+        // Create a div for the currency
+        const currencyDiv = document.createElement('div')
+        currencyDiv.className = 'label'
+        currencyDiv.textContent = `${key} ${value}`
+
+        // Random number between -1 and 1
+        const random = Math.random() * 2 - 1
+
+        // Create a CSS2DObject for the currency
+        const currencyObject = new CSS2DObject(currencyDiv)
+        currencyObject.position.x = Math.sin(Math.random() * 2 * Math.PI)
+        currencyObject.position.y = Math.random() * 2 - 1
+        currencyObject.position.z = Math.cos(Math.random() * 2 * Math.PI)
+
+        // Add the currency to the scene
+        scene.add(currencyObject)
+    }
+}
+
+
+
+const currencyDivs = {
+    AUD: document.createElement('div'),
+    BGN: document.createElement('div'),
+    BRL: document.createElement('div'),
+    CAD: document.createElement('div'),
+    CHF: document.createElement('div'),
+    CNY: document.createElement('div'),
+    CZK: document.createElement('div'),
+    EUR: document.createElement('div'),
+    GBP: document.createElement('div'),
+    HKD: document.createElement('div'),
+    JPY: document.createElement('div'),
+    MXN: document.createElement('div'),
+    PLN: document.createElement('div'),
+    RUB: document.createElement('div'),
+    USD: document.createElement('div'),
+}
+
+const currencyObjects = {
+    AUD: new CSS2DObject(currencyDivs.AUD),
+    BGN: new CSS2DObject(currencyDivs.BGN),
+    BRL: new CSS2DObject(currencyDivs.BRL),
+    CAD: new CSS2DObject(currencyDivs.CAD),
+    CHF: new CSS2DObject(currencyDivs.CHF),
+    CNY: new CSS2DObject(currencyDivs.CNY),
+    CZK: new CSS2DObject(currencyDivs.CZK),
+    EUR: new CSS2DObject(currencyDivs.EUR),
+    GBP: new CSS2DObject(currencyDivs.GBP),
+    HKD: new CSS2DObject(currencyDivs.HKD),
+    JPY: new CSS2DObject(currencyDivs.JPY),
+    MXN: new CSS2DObject(currencyDivs.MXN),
+    PLN: new CSS2DObject(currencyDivs.PLN),
+    RUB: new CSS2DObject(currencyDivs.RUB),
+    USD: new CSS2DObject(currencyDivs.USD),
+}
+
+
+```
--- main.js
+++ main.js
@@ -4,6 +4,74 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 import {GUI} from 'three/addons/libs/lil-gui.module.min.js'
 import './style.css'
 
+const apiKey = 'MXOPiCmyl3TzlByq7DuKDkRHXW0bletn4VOxFibf'
+const currencyList = 'AUD,CAD,CHF,CNY,EUR,HKD,JPY,MXN,RUB,USD'
+let currencyBase = 'AUD'
+
+
+// If developement mode, use mock data
+if (process.env.NODE_ENV === 'developments')
+    apiUrl = `${window.location.href}/fixtures/mock.json`
+
+
+// Get current values of currency from API
+const getCurrency = async () => {
+    const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=${currencyList}&base_currency=${currencyBase}`
+    // Configure the request for json data
+    const request = new Request(apiUrl, {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+    console.log(apiUrl)
+    // Convert the response to json
+    const responseJson = await (await fetch(request)).json()
+    console.log(responseJson.data)
+
+    // Return the data
+    return responseJson.data
+}
+
+const updateLabels = (data) => {
+    data = Object.entries(data)
+    data.map((x) => currencyCoordinates.map((y) => {
+        if (y.code === x[0]) {
+            x.coords = {x: y.x, y: y.y, z: y.z}
+            return x
+        } else {
+        }
+    }))
+
+    data = data.filter(x => x.coords !== undefined)
+    data.forEach(currency => {
+        // Check if label with id  currency[0] exits
+        const label = document.getElementById(currency[0])
+        if (label) {
+            label.innerHTML = `${currency[0]}: ${currency[1].toFixed(2)}`
+        } else {
+            const currencyDiv = document.createElement('div')
+            currencyDiv.className = 'label'
+            currencyDiv.id = currency[0]
+            currencyDiv.textContent = `${currency[0]} ${currency[1].toFixed(2)}`
+            currencyDiv.style.marginTop = '-.1em'
+            const currencyLabel = new CSS2DObject(currencyDiv)
+            let point = new THREE.Vector3().setFromSphericalCoords(currency.coords.x, currency.coords.y, currency.coords.z)
+            currencyLabel.position.set(point.x, point.y, point.z)
+            earth.add(currencyLabel)
+            currencyLabel.layers.set(0)
+        }
+    })
+
+    // Reset background color of all labels
+    const labels = document.getElementsByClassName('label')
+    for (let i = 0; i < labels.length; i++) {
+        labels[i].style.backgroundColor = '#747bffaa'
+    }
+    // Change background color from current base currency
+    document.getElementById(currencyBase).style.backgroundColor = 'rgba(255, 144, 140, 0.5)'
+
+}
 
 // Texture loader
 const textureLoader = new THREE.TextureLoader()
@@ -17,49 +85,75 @@ camera.position.z = 6
 camera.layers.enableAll()
 camera.layers.toggle(1)
 
-
-
-// Light setup
-const light = new THREE.PointLight(0xffffff, 1.2, 100)
-light.position.set(3, 3, 3)
-scene.add(light)
+// Spot light setup
+const spotLight = new THREE.PointLight(0xffffff, 1.2, 100)
+spotLight.position.set(12, 12, 12)
+scene.add(spotLight)
 
 // Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, .3)
 scene.add(ambientLight)
 
-
 // Earth setup
 const earthGeometry = new THREE.SphereGeometry(1, 64, 64)
+const earthTexture = textureLoader.load('images/earth-day.jpg')
+earthTexture.minFilter = THREE.NearestFilter
+earthTexture.magFilter = THREE.NearestFilter
+earthTexture.format = THREE.RGBAFormat
+
 const earthMaterial = new THREE.MeshPhongMaterial({
     specular: 0x333333,
     shininess: 15,
-    map: textureLoader.load('earth-day.jpg'),
-    specularMap: textureLoader.load('earth-specular.jpg'),
-    normalMap: textureLoader.load('earth-normal.png'),
+    map: earthTexture,
+    specularMap: textureLoader.load('images/earth-specular.jpg'),
+    normalMap: textureLoader.load('images/earth-normal.png'),
     normalScale: new THREE.Vector2(2.85, 2.85),
-    bumpMap: textureLoader.load('earth-bump.jpg'),
-    bumpScale: 0.01,
+    bumpMap: textureLoader.load('images/earth-bump.jpg'),
+    bumpScale: 1,
 })
 const earth = new THREE.Mesh(earthGeometry, earthMaterial)
-
-// Roughness map
 earth.receiveShadow = true
 scene.add(earth)
 
+const labelRenderer = new CSS2DRenderer()
+labelRenderer.setSize(window.innerWidth, window.innerHeight)
+labelRenderer.domElement.style.position = 'absolute'
+labelRenderer.domElement.style.top = '0px'
+document.body.appendChild(labelRenderer.domElement)
+
+// x,y,z coordinates of the currencies on the earth as array
+const currencyCoordinates = [
+    {code: 'AUD', x: 1, y: 2, z: 4},
+    {code: 'CAD', x: 1, y: .75, z: -.2},
+    {code: 'CHF', x: 1, y: .73, z: 1.725},
+    {code: 'CNY', x: 1, y: 1.1, z: 3.5},
+    {code: 'EUR', x: 1, y: .79, z: 1.62},
+    {code: 'HKD', x: 1, y: 1.182, z: 3.58},
+    {code: 'JPY', x: 1, y: .97, z: 3.9},
+    {code: 'MXN', x: 1, y: 1.17, z: -.2},
+    {code: 'RUB', x: 1, y: .58, z: 2.7},
+    {code: 'USD', x: 1, y: 1, z: -.3},
+]
+
+// Add helper to show the x,y,z coordinates of the currencies
+const helper = new THREE.AxesHelper(1)
+helper.position.set(0, 0, 0)
+earth.add(helper)
+
+// Get the currency data
+getCurrency().then(data => updateLabels(data))
 
 // Add clouds to earth
-const cloudTexture = new THREE.TextureLoader().load('earth-clouds.jpg')
+const cloudTexture = new THREE.TextureLoader().load('images/earth-clouds.jpg')
 const cloudMaterial = new THREE.MeshStandardMaterial({map: cloudTexture, transparent: true, opacity: 0.5, side: THREE.DoubleSide})
 const cloudGeometry = new THREE.SphereGeometry(1.015, 64, 64)
 const clouds = new THREE.Mesh(cloudGeometry, cloudMaterial)
 clouds.castShadow = true
 scene.add(clouds)
 
-
 // Add background image within a sphere skybox
 const skyboxGeometry = new THREE.SphereGeometry(12, 100, 100)
-const skyboxTexture = new THREE.TextureLoader().load('stars-milkyway.jpg')
+const skyboxTexture = new THREE.TextureLoader().load('images/skybox-milkyway.jpg')
 const skyboxMaterial = new THREE.MeshBasicMaterial({map: skyboxTexture, side: THREE.BackSide})
 const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial)
 scene.add(skybox)
@@ -70,23 +164,42 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const labelRenderer = new CSS2DRenderer()
-labelRenderer.setSize(window.innerWidth, window.innerHeight)
-labelRenderer.domElement.style.position = 'absolute'
-labelRenderer.domElement.style.top = '0px'
-document.body.appendChild(labelRenderer.domElement)
-
 const controls = new OrbitControls(camera, labelRenderer.domElement)
-controls.minDistance = 5
-controls.maxDistance = 100
+controls.minDistance = 1.5
+controls.maxDistance = 15
+controls.enableDamping = true
+
+
+// Add selection box
+const selection = document.createElement('select')
+
+// Add options to selection box
+currencyCoordinates.forEach(currency => {
+    const option = document.createElement('option')
+    option.value = currency.code
+    option.textContent = currency.code
+    selection.appendChild(option)
+})
+selection.className = 'selection'
+
+// On change of selection box, change the currencyBase and update the currency rates
+selection.onchange = () => {
+    currencyBase = selection.value
+    getCurrency().then(data => updateLabels(data))
+}
+
+
+const selectionContainer = new CSS2DObject(selection)
+selectionContainer.position.set(1.44, -1, 0)
+earth.add(selectionContainer)
 
 
 // Render loop
 const animate = function () {
     requestAnimationFrame(animate)
 
-    earth.rotation.x += 0.0005
-    earth.rotation.y += 0.001
+    earth.rotation.x += 0.00005
+    earth.rotation.y += 0.0005
 
     // Clouds rotation is the same as the earth
     clouds.rotation.x = earth.rotation.x
@@ -97,6 +210,7 @@ const animate = function () {
     skybox.rotation.y = earth.rotation.y
 
     renderer.render(scene, camera)
+    labelRenderer.render(scene, camera)
 }
 
 animate()
@@ -115,110 +229,19 @@ window.addEventListener('resize', () => {
 
 // Rotate the light arround the earth on mouse move
 window.addEventListener('mousemove', (e) => {
-    light.position.x = Math.sin(e.clientX / window.innerWidth * 2 * Math.PI) * -3
-    light.position.z = Math.cos(e.clientX / window.innerWidth * 2 * Math.PI) * -3
-    light.position.y = -(e.clientY / window.innerHeight * 6 - 3)
+    spotLight.position.x = Math.sin(e.clientX / window.innerWidth * 2 * Math.PI) * -3
+    spotLight.position.z = Math.cos(e.clientX / window.innerWidth * 2 * Math.PI) * -3
+    spotLight.position.y = -(e.clientY / window.innerHeight * 6 - 3)
 })
 
-
-
-/*
-// Add a background image
-const loader = new THREE.TextureLoader()
-const texture = loader.load('stars-dark.jpg')
-texture.minFilter = THREE.NearestFilter
-texture.magFilter = THREE.NearestFilter
-texture.format = THREE.RGBFormat
-
-// background image crop
-texture.wrapS = THREE.RepeatWrapping
-texture.wrapT = THREE.RepeatWrapping
-texture.repeat.set(1, 1)
-scene.background = texture
-*/
-
-// Get current values of currency from API
-const apiKey = 'MXOPiCmyl3TzlByq7DuKDkRHXW0bletn4VOxFibf'
-const getCurrency = async () => {
-    // Configure the request for json data
-    const request = new Request(`https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`, {
-        method: 'GET',
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    })
-    // Convert the response to json
-    const responseJson = await (await fetch(request)).json()
-    console.log(responseJson)
-
-    // Return the data
-    return responseJson.data
-}
-// console.log(getCurrency())
-
-/*
-// Place the currency values on the earth
-const placeCurrency = async () => {
-    // Get the currency data
-    const currency = await getCurrency()
-
-    // Loop through the currency data
-    for (const [key, value] of Object.entries(currency)) {
-        // Create a sphere for the currency
-        const currencyGeometry = new THREE.SphereGeometry(0.1, 32, 32)
-        const currencyMaterial = new THREE.MeshStandardMaterial({color: 0xffffff})
-        const currencySphere = new THREE.Mesh(currencyGeometry, currencyMaterial)
-        currencySphere.castShadow = true
-
-        // Set the position of the currency
-        currencySphere.position.x = Math.sin(Math.random() * 2 * Math.PI) * 3
-        currencySphere.position.y = Math.random() * 6 - 3
-        currencySphere.position.z = Math.cos(Math.random() * 2 * Math.PI) * 3
-
-        // Add the currency to the scene
-        scene.add(currencySphere)
-    }
-}
-*/
-
-
-// Print out the currency values on the earth with CSS2DRenderer
-const placeCurrency = async () => {
-    // Get the currency data
-    const currency = await getCurrency()
-
-    // Loop through the currency data
-    for (const [key, value] of Object.entries(currency)) {
-
-        // Create a div for the currency
-        const currencyDiv = document.createElement('div')
-        currencyDiv.className = 'currency'
-        currencyDiv.textContent = `${key} ${value}`
-        console.log(`${key} ${value}`)
-
-        // Create a CSS2DObject for the currency
-        const currencyObject = new CSS2DObject(currencyDiv)
-        currencyObject.position.x = Math.sin(Math.random() * 2 * Math.PI) * 3
-        currencyObject.position.y = Math.random() * 6
-        currencyObject.position.z = Math.cos(Math.random() * 2 * Math.PI) * 33
-
-        // Add the currency to the scene
-        scene.add(currencyObject)
-    }
-}
-
-// placeCurrency()
-
-// Render the scene
-const render = () => {
-    requestAnimationFrame(render)
-    renderer.render(scene, camera)
-}
-
-
-
-
-
-// Start the render loop
-render()
-
+// Add background sound to the scene
+const listener = new THREE.AudioListener()
+camera.add(listener)
+const sound = new THREE.Audio(listener)
+const audioLoader = new THREE.AudioLoader()
+audioLoader.load('sounds/background-enterprise.mp3', function (buffer) {
+    sound.setBuffer(buffer)
+    sound.setLoop(true)
+    sound.setVolume(0.5)
+    sound.play()
+})
